0038527e0b220fc0480ec5f9acbf306f3035d34a
Aaron Cohen <aarondcohen@gmail.com>
2020-10-22 04:41:38 -0400

    Test the formatters

--- src/formatter.js
+++ src/formatter.js
@@ -1,25 +1,23 @@
 'use strict';
 
-class Formatter {
-	/**
-	 * Format number with fixed precision
-	 * @param {any} value Number value
-	 * @param {number} [decimals=0] Count of decimals
-	 * @returns {string}
-	 */
-	number(value, decimals = 0) {
-		return Number(value.toFixed(decimals)).toLocaleString();
-	}
-
-	/**
-	 * Format number with fixed precision, always show the sign
-	 * @param {any} value Number value
-	 * @param {number} [decimals=0] Count of decimals
-	 * @returns {string}
-	 */
-	numberWithSign(value) {
-		return (value > 0 ? "+" : "") + this.number(...arguments)
-	}
+/**
+ * Format number with fixed precision
+ * @param {any} value Number value
+ * @param {number} [decimals=0] Count of decimals
+ * @returns {string}
+ */
+function number(value, decimals = 0) {
+	return Number(value.toFixed(decimals)).toLocaleString();
 }
 
-module.exports = new Formatter;
+/**
+ * Format number with fixed precision, always show the sign
+ * @param {any} value Number value
+ * @param {number} [decimals=0] Count of decimals
+ * @returns {string}
+ */
+function numberWithSign(value) {
+	return (value > 0 ? "+" : "") + number(...arguments)
+}
+
+module.exports = { number, numberWithSign };
